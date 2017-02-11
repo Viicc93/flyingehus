3487b8af9954f64c96d2995701eeea92931a5cf3
Victoria Johansson <victoriajohansson93@hotmail.com>
2017-02-11 22:42:13 +0100

    js

--- wp-content/themes/flyingehus_theme/assets/js/script-min.js
+++ wp-content/themes/flyingehus_theme/assets/js/script-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$("#primarynav").addClass("nav navbar-nav")});
+$(document).ready(function(){$("#primarynav").addClass("nav navbar-nav"),$(window).on("resize",function(){$(".post-img").height($(".post-img").width()/1.5)}).resize(),$(window).on("resize",function(){$(".hero-img").height($(".hero-img").width()/1.8)}).resize()});
--- wp-content/themes/flyingehus_theme/assets/js/script.js
+++ wp-content/themes/flyingehus_theme/assets/js/script.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
 	$('#primarynav').addClass('nav navbar-nav');
 
+	$(window).on( 'resize', function () {
+			$('.post-img').height( $('.post-img').width() / 1.5 );
+	}).resize();
+	$(window).on( 'resize', function () {
+			$('.hero-img').height( $('.hero-img').width() / 1.8 );
+	}).resize();
 });
